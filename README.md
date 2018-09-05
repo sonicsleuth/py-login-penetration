f7097d5c4a8d1603b36641c62c761b9eeaf64dac
RichardSoares <soares.richard@gmail.com>
2018-09-05 11:43:26 -0400

    First release of files.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# py-login-penetration
+# Login Penetration Test
+
+## Summary: 
+* A login form penetration test written in python3.  
+* This script will submit 1000 fake users into a login form.  
+* Additionally, we assume there is a csrf_token and some cookies set during the initial load of the form which also must be submit with the form data.
+
+## Mock Data Source:
+* https://www.mockaroo.com/
+* Great resource for generating fake data for databases, etc.
+
+## Python Version:
+* Python 3.7.0
+
+## Install these packages using: pip3 install <package_name>
+* certifi (recomended by python3 for SSL certs)
+* requests (for this project)
+* BeautifulSoup (for this project) - https://www.crummy.com/software/BeautifulSoup/bs4/doc/#quick-start
+
+## Terminal Command Formats:
+* python3 <file>
+* pip3 install <package>
+
---
+++ login.py
@@ -0,0 +1,43 @@
+# Filename: login.py
+
+import requests
+import json
+from bs4 import BeautifulSoup
+
+# Load mock data (our list of fake users)
+people = json.loads(open('mock_data.json').read())
+
+# Let's create some new accounts!
+# Log our results to this file.
+f = open("log.txt", "a")
+for person in people:
+    # Load the Login Form to get our cookies and unique csrf_token.
+    print("Load Form...")
+    url_form    = 'https://domain.com/login-form'
+    response    = requests.get(url_form, allow_redirects=False)
+    cookie_data = {'SECURECOOKIE': response.cookies['SECURECOOKIE']}
+    soup        = BeautifulSoup(response.content, 'html5lib')
+    csrf_token  = soup.find('input', {'name': 'csrf'}).get('value')
+
+    # Prepare the Form POST data
+    email           = person.get("email")
+    password        = person.get("password")
+    csrf_token      = csrf_token
+
+    # Post the Form with the above Cookies
+    print("Submit Form POST...")
+    url_post = 'https://domain.com/login-form'
+
+    result = requests.post(url_post, allow_redirects=False, cookies=cookie_data, data={
+        'email': email,
+        'password': password,
+        'csrf': csrf
+    })
+    # Terminal Output
+    print("(" + email + " / " + password + ")")
+    print(str(result.content))
+    # Log Output
+    f.write("Email: " + email + " Password: " + password + "\n")
+    f.write(str(result.content) + "\n")
+
+# END-OF-FILE
